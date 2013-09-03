68b9239d78adb82f565dbf38827c4b73266b8798
vadymGoncharenko <vdmgn@ukr.net>
2013-09-03 09:26:15 +0300

    added proxy to users.json file

--- 1 project/app/store/Users.js
+++ 1 project/app/store/Users.js
@@ -3,17 +3,17 @@ Ext.define("AM.store.Users", {
     model:'AM.model.User',
     autoLoad: true,
 
-    data: [
-        {name: 'Ed',    email: 'ed@sencha.com'},
-        {name: 'Tommy', email: 'tommy@sencha.com'}
-    ]
-//    proxy: {
-//        type: 'ajax',
-//        url: 'data/users.json',
-//        reader: {
-//            type: 'json',
-//            root: 'users',
-//            successProperty: 'success'
-//        }
-//    }
+    //data: [
+    //    {name: 'Ed',    email: 'ed@sencha.com'},
+    //    {name: 'Tommy', email: 'tommy@sencha.com'}
+    //]
+    proxy: {
+        type: 'ajax',
+        url: 'data/users.json',
+       reader: {
+            type: 'json',
+           root: 'users',
+            successProperty: 'success'
+        }
+    }
 })
